6442cedfab67f984f5ed66e55de4a57fb4fe2fea
Gustavo Siqueira <gus@brid-it.com>
2023-02-20 11:56:54 -0300

    laravel 10 upgrade

--- src/Infrastructure/Database/Seeders/DatabaseSeeder.php
+++ src/Infrastructure/Database/Seeders/DatabaseSeeder.php
@@ -2,18 +2,21 @@
 
 namespace App\Infrastructure\Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         // \App\Infrastructure\Models\UserModel::factory(10)->create();
+
+        // \App\Infrastructure\Models\UserModel::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
